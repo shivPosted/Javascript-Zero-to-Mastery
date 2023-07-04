37946c5989b8b75cc226decf5c5e90a9d95e4931
shivPosted <shivpratap621@gmail.com>
2023-07-04 23:09:26 +0530

    completed assignment-02 Data-types

--- 01-Fundamentals-Part1/assignments.js
+++ 01-Fundamentals-Part1/assignments.js
@@ -6,8 +6,23 @@
 let country = "India";
 let continent = "Asia";
 let population = "1,406 millions"
+// console.log("result of assignment-01")
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-console.log("result of assignment-01")
-console.log(country);
-console.log(continent);
-console.log(population);
+
+//Assignment-02
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let language;
+console.log("assignment");
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -1,15 +1,30 @@
-let js = "amazing";
+// let js = "amazing";
 
-let firstName = "Shiv";
-let lastName = "Pratap";
-let PI = 3.1415;                        //for constant values use  all caps
+// let firstName = "Shiv";
+// let lastName = "Pratap";
+// let PI = 3.1415;                        //for constant values use  all caps
 
-console.log(js);
-console.log(firstName);
-console.log(lastName);
+// console.log(js);
+// console.log(firstName);
+// console.log(lastName);
 
-console.log(3000);
+// console.log(3000);
 
-let $result = "fail";                   //can only use $ sign in variable name in special characters
-console.log($result);
+// let $result = "fail";                   //can only use $ sign in variable name in special characters
+// console.log($result);
+
+let isJsFun = true;
+
+console.log(isJsFun);
+console.log(typeof isJsFun);
+
+isJsFun = "Yes";                                            //example of dynamic typing in js
+console.log(isJsFun)
+console.log(typeof isJsFun);
+
+let string01;                                       //undefined value
+console.log(string01);
+console.log(typeof string01);
+
+console.log(typeof null);                                           // a bug in JS that null is an object type
 
