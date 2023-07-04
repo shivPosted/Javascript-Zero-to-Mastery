fed18250c9f7d639c66c57db95f2612484111b42
shivPosted <shivpratap621@gmail.com>
2023-07-04 22:36:22 +0530

    completed assignment-01

---
+++ 01-Fundamentals-Part1/assignments.js
@@ -0,0 +1,13 @@
+//assignment-01
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "India";
+let continent = "Asia";
+let population = "1,406 millions"
+
+console.log("result of assignment-01")
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -1,6 +1,15 @@
 let js = "amazing";
-if (js === "amazing") {
-    alert("JS is amazing!");
-    let result = 40 + 20 - 15 + 56;
-    console.log(result)
-}
+
+let firstName = "Shiv";
+let lastName = "Pratap";
+let PI = 3.1415;                        //for constant values use  all caps
+
+console.log(js);
+console.log(firstName);
+console.log(lastName);
+
+console.log(3000);
+
+let $result = "fail";                   //can only use $ sign in variable name in special characters
+console.log($result);
+
