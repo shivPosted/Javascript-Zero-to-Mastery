3d039d37430b47db9523cca4aed2932e0f377c9c
shivPosted <shivpratap621@gmail.com>
2023-07-05 12:31:08 +0530

    Challenge#01 completed and learned operators precedence

--- 01-Fundamentals-Part1/assignments.js
+++ 01-Fundamentals-Part1/assignments.js
@@ -66,3 +66,32 @@ let populationLessThanAverage = 33 > population;
 console.log("The population of my country is less than average is " + populationLessThanAverage);
 let description = country + " is in " + continent + ", and its " + population + " million people speaks " + language;
 console.log(description);
+
+console.log("Challenge#01");
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+// Your task is to write some code to help them:
+
+// Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+// Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+// Log the value of BMIMark and BMIJohn to the console.
+
+// BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+// TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+/* Write your code below. Good luck! 🙂 */
+let massMark = 78;
+let massJohn = 92;
+let heightMark = 1.69;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log("Mark's BMI: " + BMIMark + " John's BMI: " + BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+
+console.log("Is Mark's BMI higher than John is " + markHigherBMI);
