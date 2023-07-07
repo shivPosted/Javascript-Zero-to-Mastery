2742356bd4137c1ca28cdbd50271d02ce1989e45
shivPosted <shivpratap621@gmail.com>
2023-07-07 14:09:02 +0530

    Assgnment-22 ✅

--- 02-Fundamentals-Part2/assignment.js
+++ 02-Fundamentals-Part2/assignment.js
@@ -128,7 +128,7 @@ const scoreDolphins = calcAverage(44, 23, 71);
 const scoreKoalas = calcAverage(65, 54, 49);
 
 const checkWinner = (avgDolphins, avgKoalas) => {
-    
+
     if (avgDolphins >= avgKoalas*2){
         console.log(`Dolphins win (${avgeDolphins} vs. ${avgKoalas})`);
     }
@@ -294,7 +294,7 @@ console.log(answer);*/
 
 
 
-console.log('Assignment-21');
+/*console.log('Assignment-21');
 
 // 1. There are elections in your country! In a small town, there are only 50 voters.
 // Use a for loop to simulate the 50 people voting, by logging a string like this to
@@ -302,4 +302,30 @@ console.log('Assignment-21');
 
 for (let i = 0; i < 50; i++) {
     console.log(`Voter number ${i + 1} is currently voting`);
+}*/
+
+
+
+
+console.log('Assignment-21');
+
+// 1. Let's bring back the 'populations' array from a previous assignment
+// 2. Use a for loop to compute an array called 'percentages2' containing the
+// percentages of the world population for the 4 population values. Use the
+// function 'percentageOfWorld1' that you created earlier
+// 3. Confirm that 'percentages2' contains exactly the same values as the
+// 'percentages' array that we created manually in the previous assignment,
+// and reflect on how much better this solution is
+
+const POPULATIONS = [1440, 50, 100, 95];
+const percentage2 = [];
+for (let i = 0; i < POPULATIONS.length; i++) {
+    percentage2.push(percentageOfWorld1(i));
 }
+
+function percentageOfWorld1(index) {
+    const percentage = (POPULATIONS[index] / 7490) * 100;
+    return percentage;
+}
+
+console.log(percentage2);
