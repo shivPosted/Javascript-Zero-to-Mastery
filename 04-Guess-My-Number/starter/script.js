4f66bee3f10c792612859f20a0cb6282913941b5
shivPosted <shivpratap621@gmail.com>
2023-07-08 22:53:49 +0530

    Done with Design and structure ✅

--- 04-Guess-My-Number/starter/script.js
+++ 04-Guess-My-Number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//Guess my number using prompt
+
+// const luckyNumber = 13;
+// let count = Number(prompt('Enter the number between 1-20'));
+// let counter = 1;
+// while (count !== luckyNumber) {
+//   if (count > luckyNumber) {
+//     count = Number(prompt('Too high enter again'));
+//     counter++;
+//     continue;
+//   } else if (count < luckyNumber) {
+//     count = Number(prompt('Too low enter again'));
+//     counter++;
+//     continue;
+//   }
+// }
+
+// alert(`Congrats You won, you took ${counter} tries to win`);
