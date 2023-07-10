0f478d0a6bf801c07fa53394eb89bccddfffc6a2
shivPosted <shivpratap621@gmail.com>
2023-07-10 19:47:23 +0530

    Open modal project, closing the modal-window ✅

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -1,13 +1,23 @@
 'use strict';
 const openModal = document.querySelector('.modal-window'); //storing element in a constant
 const openModalButtons = document.querySelectorAll('.modal-button'); //select all elements with the same class
-const close = document.querySelector('.close-modal');
+const buttonClose = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
+const openModalWindow = function () {
+  openModal.classList.remove('hidden'); //this will remove the hidden class of the modal-window
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  openModal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+// --> this loop will attach the event handlers to all of the buttons by looping over the nodelist
 for (let i = 0; i < openModalButtons.length; i++) {
-  // --> this function will attach the event handlers to all of the buttons by looping over the nodelist
-  openModalButtons[i].addEventListener('click', function () {
-    openModal.classList.remove('hidden'); //this will remove the hidden class of the modal-window
-    overlay.classList.remove('hidden');
-  });
+  openModalButtons[i].addEventListener('click', openModalWindow);
 }
+
+buttonClose.addEventListener('click', closeModal); //using closeModal as even-handler function, don't use () with closeModal because it will make function rum immedialtely when page loads
+
+overlay.addEventListener('click', closeModal);
