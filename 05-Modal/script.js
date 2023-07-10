f9fe577239dfaf1130306bada61af320cc475aae
shivPosted <shivpratap621@gmail.com>
2023-07-10 22:12:59 +0530

    Project Complete 💯

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -21,3 +21,13 @@ for (let i = 0; i < openModalButtons.length; i++) {
 buttonClose.addEventListener('click', closeModal); //using closeModal as even-handler function, don't use () with closeModal because it will make function rum immedialtely when page loads
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //whenever an event occur an object is generated which will store info about the object, we can access that object in the even-handler function, In this case that event is e as a parameter
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !openModal.classList.contains('hidden')) {
+    // using function call instead of reference because we are not passing as an argument
+    closeModal();
+  }
+});
