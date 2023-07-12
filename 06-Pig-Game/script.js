ce3866f67325ac3c699189c9c9e3fcfdc3b37adb
shivPosted <shivpratap621@gmail.com>
2023-07-12 21:50:19 +0530

    function- new-game and refactoring the code 🎲

--- 06-Pig-Game/script.js
+++ 06-Pig-Game/script.js
@@ -1,6 +1,4 @@
 'use strict';
-const score01El = document.getElementById('score--1');
-const score02El = document.getElementById('score--2');
 const newGame = document.querySelector('.new-game');
 const holdGame = document.querySelector('.pause-game');
 const rollDice = document.querySelector('.roll-dice');
@@ -8,11 +6,25 @@ const current01El = document.getElementById('current--1');
 const current02El = document.getElementById('current--2');
 const diceImage = document.querySelector('.dice-image');
 
-let currentScore = 0;
-let isPlaying = true;
-let activePlayer = 1;
-let scores = [0, 0];
+let currentScore, isPlaying, activePlayer, scores;
 
+const init = function () {
+  isPlaying = true;
+  currentScore = 0;
+  scores = [0, 0];
+  activePlayer = 1;
+  document.querySelector(`.player-1`).classList.remove('player-inactive');
+  document.querySelector(`.player-2`).classList.add('player-inactive');
+  document.querySelector(`.total-score-1`).textContent = 0;
+  document.querySelector(`.total-score-2`).textContent = 0;
+  current01El.textContent = 0;
+  current02El.textContent = 0;
+  diceImage.classList.add('hidden');
+  document.querySelector(`.player-1`).classList.remove('player-won');
+  document.querySelector(`.player-2`).classList.remove('player-won');
+};
+
+init();
 const switchPlayer = function () {
   currentScore = 0;
   document.querySelector(`.current-score-${activePlayer}`).textContent =
@@ -21,11 +33,9 @@ const switchPlayer = function () {
     .querySelector(`.player-${activePlayer}`)
     .classList.toggle('player-inactive');
   activePlayer = activePlayer === 1 ? 2 : 1;
-  document
-    .querySelector(`.player-${activePlayer}`)
-    .classList.toggle('player-inactive');
 };
-rollDice.addEventListener('click', function () {
+
+const ROLL_DICE = function () {
   if (isPlaying) {
     const dice = Math.trunc(Math.random() * 6) + 1;
     diceImage.classList.remove('hidden');
@@ -37,10 +47,10 @@ rollDice.addEventListener('click', function () {
         currentScore; //changes to be made for different players
     } else switchPlayer();
   }
-});
-
-holdGame.addEventListener('click', function () {
+};
+const HOLD = function () {
   if (isPlaying) {
+    diceImage.classList.add('hidden');
     scores[activePlayer - 1] += currentScore;
     console.log(scores[activePlayer - 1]);
     document.querySelector(`.total-score-${activePlayer}`).textContent =
@@ -49,8 +59,16 @@ holdGame.addEventListener('click', function () {
       document
         .querySelector(`.player-${activePlayer}`)
         .classList.add('player-won');
+      document.querySelector(
+        `.celebrate-${activePlayer}`
+      ).textContent = `Player ${activePlayer} won`;
       isPlaying = false;
-      diceImage.classList.add('hidden');
     } else switchPlayer();
   }
-});
+};
+
+rollDice.addEventListener('click', ROLL_DICE);
+
+holdGame.addEventListener('click', HOLD);
+
+newGame.addEventListener('click', init);
