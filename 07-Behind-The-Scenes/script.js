cb8e9ac04c274ceca9e4c6520717570444a2b0fd
shivPosted <shivpratap621@gmail.com>
2023-07-14 00:51:45 +0530

    Scope and Scope chaining 🤔

--- 07-Behind-The-Scenes/script.js
+++ 07-Behind-The-Scenes/script.js
@@ -1,1 +1,75 @@
 'use strict';
+
+var x = 5;
+
+function myFunction() {
+  var y = 10;
+  console.log(x); // What will be printed?
+}
+
+myFunction();
+
+function outerFunction() {
+  var x = 5;
+
+  function innerFunction() {
+    var y = 10;
+    console.log(x); // What will be printed?
+  }
+
+  innerFunction();
+}
+
+outerFunction();
+
+function myFunction2() {
+  if (true) {
+    var x = 5;
+    let y = 10;
+  }
+
+  console.log(x); // What will be printed?
+  // console.log(y); // What will be printed?
+}
+
+myFunction2();
+
+var x = 5;
+
+var myFunction3 = function () {
+  var x = 10;
+  console.log(x); // What will be printed?
+};
+
+myFunction3();
+
+function outerFunction1() {
+  var x = 5;
+
+  function innerFunction() {
+    console.log(x); // What will be printed?
+  }
+
+  return innerFunction;
+}
+
+var inner = outerFunction1();
+inner();
+
+function outerFunction2() {
+  var x = 10;
+
+  function innerFunction() {
+    var y = 5;
+
+    function deepInnerFunction() {
+      console.log(x + y); // What will be printed?
+    }
+
+    deepInnerFunction();
+  }
+
+  innerFunction();
+}
+
+outerFunction2();
