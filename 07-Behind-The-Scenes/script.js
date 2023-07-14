15981115fc5e0ed1fa327b9348fa6eb7e01c2cce
shivPosted <shivpratap621@gmail.com>
2023-07-14 15:52:24 +0530

    The this keyword 🤔

--- 07-Behind-The-Scenes/script.js
+++ 07-Behind-The-Scenes/script.js
@@ -91,7 +91,51 @@ function myFunction4() {
 //   console.log('Hello!');
 // };
 
-if (true) {
-  console.log(x); // What will happen?
-  let x = 5;
+// if (true) {
+//   console.log(x); // What will happen?
+//   let x = 5;
+// }
+
+/*this keyword*/
+
+console.log(this);
+
+function calcAge(birthYear) {
+  console.log(2043 - birthYear);
+  console.log(this); //will be undefined only in strict mode
 }
+calcAge(2001);
+
+const calcAgeArrow = birthYear => {
+  console.log(2043 - birthYear);
+  console.log(this); //will be window object because arrow function doesn't have their own this keyword but get it from their parent in this case window object
+};
+
+calcAgeArrow(2003);
+
+const f = function () {
+  console.log(this.name);
+  console.log(this.year);
+  console.log(2037 - this.year);
+};
+
+const shiv = {
+  firstName: 'Shiv',
+  year: 2001,
+  calcAge: f,
+};
+
+shiv.calcAge(); //this will point to the object which call the method
+
+const john = {
+  name: 'john',
+  year: 2005,
+};
+
+john.calcAge = shiv.calcAge;
+
+console.log(john);
+
+john.calcAge();
+
+f();
