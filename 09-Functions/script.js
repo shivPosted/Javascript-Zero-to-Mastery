0e7659013bbc1ee9c7e3a297f2634c1b36aab5e3
shivPosted <shivpratap621@gmail.com>
2023-07-19 02:39:42 +0530

    Functions calling callback functions 🤔

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -18,7 +18,7 @@ const bookFlight = function (
 
 bookFlight('BNB2310', undefined, 30, undefined);*/
 
-const flightName = 'LH345';
+/*const flightName = 'LH345';
 const shiv = {
   name: 'Shiv Pratap',
   passport: 3256417852,
@@ -29,4 +29,49 @@ const checkIn = function (flight, person) {
   person.name = 'Mr. ' + person.name; //---------------->> will change the main object too because objects are stored in heaps and not in call-stack;
 };
 checkIn(flightName, shiv);
-console.log(flightName, shiv);
+console.log(flightName, shiv);*/
+
+//Callback function and higher order function
+//example-1
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+const calculator = function (a, b, fn) {
+  return `Result of the operation is: ${fn(a, b)}`;
+};
+
+console.log(calculator(5, 7, divide));
+
+//example-2
+function filterEven(arr) {
+  for (const [key, value] of arr.entries()) {
+    console.log(key, value);
+    if (value % 2 === 0) {
+      arr.splice(key, 1);
+    }
+  }
+  return arr;
+}
+
+const filterArray = function (arr, fn) {
+  return `The filtered array is: ${fn(arr)}`;
+};
+console.log(
+  filterArray(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+    filterEven
+  )
+);
