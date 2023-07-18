80a8aeafa14cde882fa13af0b867b0194f0c3552
shivPosted <shivpratap621@gmail.com>
2023-07-18 23:16:30 +0530

    Practicing string methods...🤐

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const Days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
@@ -356,3 +352,20 @@ button.addEventListener('click', function () {
     console.log(`${value.padEnd(25, ' ')}${'✅'.repeat(key + 1)}`);
   }
 });
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  console.log(
+    `${
+      type.includes('Delayed')
+        ? '🔴 ' + type.replaceAll('_', '')
+        : type.replaceAll('_', '')
+    } from ${from.slice(0, 3).toUpperCase()} to ${to
+      .slice(0, 3)
+      .toUpperCase()} (${time.replace(':', 'h')})`.padStart(48, ' ')
+  );
+}
