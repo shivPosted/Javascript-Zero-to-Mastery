fce0ac4e1e3dbb6cde018d79767c8d5727cc76e0
shivPosted <shivpratap621@gmail.com>
2023-07-19 01:00:33 +0530

    Function default parameters 🤔

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const passangers = [];
+const bookFlight = function (flightName, flightNumber = 10, passanger = 20) {
+  const bookings = {
+    flightName,
+    flightNumber,
+    passanger,
+  };
+  console.log(bookings);
+};
+
+bookFlight('BNB2310', undefined, 30);
