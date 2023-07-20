e2caaeca2bf7bfe3b400479debe185c89eb0a6fb
shivPosted <shivpratap621@gmail.com>
2023-07-20 17:07:43 +0530

    Functions- Challenge#2 done 🤔

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -297,3 +297,21 @@ const demo = function (n, groupStrength, timeInSecond) {
 };
 
 demo(500, 50, 5);
+
+// Coding Challenge #2
+// This is more of a thinking challenge than a coding challenge 🤓
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})(); // IIFE will be popped out after executing for once but a closure will be created for the eventHandler function that will carry the value of header variable even after iife is done executing
