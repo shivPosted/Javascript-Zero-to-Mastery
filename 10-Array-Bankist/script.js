40fe08428d8809517af939fce511ed1d93b9836d
shivPosted <shivpratap621@gmail.com>
2023-07-26 19:30:27 +0530

    Computing username using map method 🚀

--- 10-Array-Bankist/script.js
+++ 10-Array-Bankist/script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Shiv Pratap',
+  owner: 'Shiv Pratap Singh',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Krishna Pratap',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Rengoku Kyojiro',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Matsumoto Rangiku',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -79,14 +79,20 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+//Computing username using map method
 const user = account1.owner;
-const userName = user
-  .toLowerCase()
-  .split(' ')
-  .map(elem => elem[0])
-  .join('');
+const userName = function (accs) {
+  accs.forEach(function (acc) {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(elem => elem[0])
+      .join('');
+  });
+};
 
-console.log(userName);
+userName(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
