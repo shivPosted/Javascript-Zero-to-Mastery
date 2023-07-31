5da4c6d287ffe98434c32bd869ab27b8a9398d65
shivPosted <shivpratap621@gmail.com>
2023-07-31 21:05:21 +0530

    Array-Cardio #2 done 🚀

--- 10-Array-Bankist/exercise.js
+++ 10-Array-Bankist/exercise.js
@@ -325,3 +325,36 @@ const sortedByRecommended = dogs.sort(
   (a, b) => a.recommendedFood - b.recommendedFood
 );
 console.log(sortedByRecommended);
+
+// ## Array Cardio Day 2
+
+const people01 = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 },
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 },
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+console.log(people01.some(current => 2023 - current.year >= 19));
+// Array.prototype.every() // is everyone 19 or older?
+console.log(people01.every(current => 2023 - current.year >= 19));
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+console.log(comments.find(current => current.id === 823423));
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+console.log(comments.findIndex(current => current.id === 823423));
