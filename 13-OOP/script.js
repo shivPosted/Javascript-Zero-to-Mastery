1e372e450aadc61b99fb787e8ced63a129707e15
shivPosted <shivpratap621@gmail.com>
2023-08-22 00:46:03 +0530

    Prototypes and prototypal inheritance 🥲

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -5,9 +5,9 @@
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
-  this.calcAge = function () {
-    return 2037 - birthYear;
-  };
+  // this.calcAge = function () {
+  //   return 2037 - birthYear;
+  // };
 };
 
 //  4-steps that happen when we call a constructor function
@@ -23,3 +23,24 @@ console.log(shiv);
 const vishnu = new Person('Vishnu', 1998);
 const brahma = new Person('Brahma', 1995);
 console.log(vishnu, brahma);
+
+//Prototypes
+Person.prototype.calcAge = function () {
+  //will set this function as the prototype property of the Person which can then be used by its instances
+  console.log(2037 - this.birthYear);
+};
+console.log(Person.prototype);
+
+shiv.calcAge();
+vishnu.calcAge();
+brahma.calcAge();
+
+console.log(shiv.__proto__);
+console.log(shiv.__proto__ === Person.prototype); //as __proto__ is just a copy of the Person.prototype
+console.log(Person.prototype.isPrototypeOf(Person)); // Person.prototype is not a prototype of Person but a property of it
+console.log(Person.prototype.isPrototypeOf(shiv)); // it is true
+console.log(shiv.hasOwnProperty('calcAge'));
+
+Person.prototype.species = 'Homo Sepians';
+console.log(shiv.species, vishnu.species);
+console.log(shiv.hasOwnProperty('species'));
