5cc6d323995c0550f4fb30981c376978583de869
shivPosted <shivpratap621@gmail.com>
2023-08-22 22:49:51 +0530

    coding challenge#1 done 🚀

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -44,3 +44,37 @@ console.log(shiv.hasOwnProperty('calcAge'));
 Person.prototype.species = 'Homo Sepians';
 console.log(shiv.species, vishnu.species);
 console.log(shiv.hasOwnProperty('species'));
+
+// Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+// 'speed' property. The 'speed' property is the current speed of the car in
+// km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10,
+// and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+// the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+// 'brake' multiple times on each of them
+// Test data:
+// § Data car 1: 'BMW' going at 120 km/h
+// § Data car 2: 'Mercedes' going at 95 km/h
+
+const Car = function (make, speed) {
+  (this.make = make), (this.speed = speed);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+Car.prototype.accelerate = function () {
+  console.log(this.speed + 10);
+};
+Car.prototype.brake = function () {
+  console.log(this.speed - 5);
+};
+
+car1.accelerate();
+car2.accelerate();
+car1.brake();
+car2.brake();
