7fd97adb7ac00ee58c8c563fdc2d5f01cd3c1b34
shivPosted <shivpratap621@gmail.com>
2023-09-02 02:40:14 +0530

    use of leaflet Library 😁

--- 14-Mapty/script.js
+++ 14-Mapty/script.js
@@ -14,10 +14,24 @@
 navigator.geolocation.getCurrentPosition(
   function (position) {
     // console.log(position);
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
+    const { latitude, longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
     console.log(latitude, longitude);
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    var map = L.map('map').setView(coords, 17);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
     alert(`couldn't get your locatoin`);
