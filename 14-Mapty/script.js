a90b639389259b2b95a79da1845f2f6187db00ce
shivPosted <shivpratap621@gmail.com>
2023-09-03 03:13:40 +0530

    rendering input form 😁

--- 14-Mapty/script.js
+++ 14-Mapty/script.js
@@ -3,14 +3,13 @@
 // // prettier-ignore
 // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-// const form = document.querySelector('.form');
-// const containerWorkouts = document.querySelector('.workouts');
-// const inputType = document.querySelector('.form__input--type');
-// const inputDistance = document.querySelector('.form__input--distance');
-// const inputDuration = document.querySelector('.form__input--duration');
-// const inputCadence = document.querySelector('.form__input--cadence');
-// const inputElevation = document.querySelector('.form__input--elevation');
-
+const form = document.querySelector('.form');
+const inputDistance = document.getElementById('distance');
+const inputDuration = document.getElementById('duration');
+const inputCadence = document.getElementById('cadence');
+const inputElevGain = document.getElementById('elev-gain');
+const inputType = document.getElementById('type');
+let map, mapEvent;
 //use of geolocation
 navigator.geolocation.getCurrentPosition(
   function (position) {
@@ -23,30 +22,14 @@ navigator.geolocation.getCurrentPosition(
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     //leaflet library
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
-    map.on('click', function (mapEvent) {
+    map.on('click', function (mapE) {
+      //rendering form visible when clicked on map
+      form.classList.remove('hidden');
+      inputDistance.focus();
+      mapEvent = mapE;
       //for hannling anything that happen on map
-      console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
-      const myIcon = L.icon({
-        iconUrl: 'icon.png',
-        iconSize: [45, 45],
-        iconAnchor: [20, 50],
-        popupAnchor: [0, -50],
-      });
-
-      L.marker([lat, lng], { icon: myIcon })
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            autoClose: false,
-            closeOnClick: false,
-            className: 'cycling-popup',
-          })
-        )
-        .setPopupContent('workout')
-        .openPopup();
     });
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -58,3 +41,41 @@ navigator.geolocation.getCurrentPosition(
     alert(`couldn't get your locatoin`);
   }
 );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevGain.value =
+      '';
+
+  console.log('submitted');
+  const { lat, lng } = mapEvent.latlng;
+  const myIcon = L.icon({
+    iconUrl: 'icon.png',
+    iconSize: [45, 45],
+    iconAnchor: [20, 50],
+    popupAnchor: [0, -50],
+  });
+
+  L.marker([lat, lng], { icon: myIcon })
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        autoClose: false,
+        closeOnClick: false,
+        className: 'cycling-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+
+  form.classList.add('hidden');
+});
+
+type.addEventListener('change', function () {
+  inputElevGain.closest('.form-row').classList.toggle('form-row-hidden');
+  inputCadence.closest('.form-row').classList.toggle('form-row-hidden');
+  console.log(inputCadence.closest('.form-row'));
+});
