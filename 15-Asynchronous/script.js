116f08f593626b91fbc2f8e4f2592ce61f9f98c5
shivPosted <shivpratap621@gmail.com>
2023-09-07 16:54:39 +0530

    refactoring the code 🧵

--- 15-Asynchronous/script.js
+++ 15-Asynchronous/script.js
@@ -68,41 +68,42 @@ const imageUpdate = document.querySelector('.images img');
 //     imageUpdate.src = images;
 //   });
 // };
+
+const setElCountry = function (data, isNeighbour = false) {
+  let languages = '';
+  for (const [, value] of Object.entries(data.languages)) {
+    languages += value + ' ';
+  }
+  const [, currencyAry] = Object.entries(data.currencies)[0];
+
+  const html = `<div class="country ${isNeighbour ? 'country-neighbour' : ''}">
+<img src="${data.flags.png}" alt="country-flag" />
+<div class="country-data">
+  <h3 class="country-row country-name">${data.name.common}</h3>
+  <h5 class="country-row country-continent">${data.continents[0]}</h5>
+  <h5 class="country-row country-population">🧑‍🤝‍🧑 ${(
+    data.population / 1000000
+  ).toFixed(1)} M</h5>
+  <h5 class="country-row country-language">🗣️ ${languages}</h5>
+  <h5 class="country-row country-currency">💵 ${currencyAry.name}</h5>
+</div>
+</div>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
 const getNeighbour = function (data) {
-  console.log(data);
   if (data) {
-    console.log(data);
     const request02 = new XMLHttpRequest();
     request02.open('GET', `https://restcountries.com/v3.1/alpha/${data[0]}`);
     request02.send();
     request02.addEventListener('load', function () {
-      let languages = '';
       const [data02] = JSON.parse(request02.responseText);
-      console.log(data02);
-      for (const [, value] of Object.entries(data02.languages)) {
-        languages += value + ' ';
-      }
-      const [, currencyAry] = Object.entries(data02.currencies)[0];
-
-      const html = `<div class="country country-neighbour">
-    <img src="${data02.flags.png}" alt="country-flag" />
-    <div class="country-data">
-      <h3 class="country-row country-name">${data02.name.common}</h3>
-      <h5 class="country-row country-continent">${data02.continents[0]}</h5>
-      <h5 class="country-row country-population">🧑‍🤝‍🧑 ${(
-        data02.population / 1000000
-      ).toFixed(1)} M</h5>
-      <h5 class="country-row country-language">🗣️ ${languages}</h5>
-      <h5 class="country-row country-currency">💵 ${currencyAry.name}</h5>
-    </div>
-  </div>`;
-      countriesContainer.insertAdjacentHTML('beforeend', html);
+      setElCountry(data02, true);
     });
   }
 };
 
 const getCountry = function (country) {
-  let languages = '';
   const request = new XMLHttpRequest(); //request object from the XMLHttpsRequest
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`); //to open a request accpts arguments for type of request and source for request
   request.send(); //for sending request
@@ -114,25 +115,7 @@ const getCountry = function (country) {
     console.log(data?.borders);
 
     // console.log(data);
-    for (const [, value] of Object.entries(data.languages)) {
-      languages += value + ' ';
-    }
-    const [, currencyAry] = Object.entries(data.currencies)[0];
-
-    const html = `<div class="country">
-  <img src="${data.flags.png}" alt="country-flag" />
-  <div class="country-data">
-    <h3 class="country-row country-name">${data.name.common}</h3>
-    <h5 class="country-row country-continent">${data.continents[0]}</h5>
-    <h5 class="country-row country-population">🧑‍🤝‍🧑 ${(
-      data.population / 1000000
-    ).toFixed(1)} M</h5>
-    <h5 class="country-row country-language">🗣️ ${languages}</h5>
-    <h5 class="country-row country-currency">💵 ${currencyAry.name}</h5>
-  </div>
-</div>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
+    setElCountry(data, false);
     getNeighbour(data?.borders); //example of callback
   });
 };
@@ -140,15 +123,15 @@ const getCountry = function (country) {
 getCountry('india');
 
 //another example of callback
-setTimeout(() => {
-  console.log('hello01');
-  setTimeout(() => {
-    console.log('hello02');
-    setTimeout(() => {
-      console.log('hello03');
-      setTimeout(() => {
-        console.log('hello04');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log('hello01');
+//   setTimeout(() => {
+//     console.log('hello02');
+//     setTimeout(() => {
+//       console.log('hello03');
+//       setTimeout(() => {
+//         console.log('hello04');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
