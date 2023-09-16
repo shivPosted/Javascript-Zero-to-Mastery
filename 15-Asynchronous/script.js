cedcb7212eec664dddbed7b1c290aec33249785a
shivPosted <shivpratap621@gmail.com>
2023-09-16 23:04:32 +0530

    event-loop and microtasks precedence 🧵

--- 15-Asynchronous/script.js
+++ 15-Asynchronous/script.js
@@ -244,4 +244,8 @@ btn.addEventListener('click', () => {
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Promise 1 resolved').then(res => console.log(res));
+Promise.resolve('Promise 2 resolved').then(res => {
+  for (let i = 0; i < 10000000000; i++) {}
+  console.log(res);
+});
 console.log('Test End');
