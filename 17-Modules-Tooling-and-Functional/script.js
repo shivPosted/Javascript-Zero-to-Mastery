9c47903c842663a49c90ebc243fcb749a0ebca81
shivPosted <shivpratap621@gmail.com>
2023-09-30 00:19:51 +0530

    using all the exports at the same time

--- 17-Modules-Tooling-and-Functional/script.js
+++ 17-Modules-Tooling-and-Functional/script.js
@@ -1,6 +1,10 @@
 //importing module
-import { addToCart as AC, shoppingCart as SC } from './shoppingCart.js';
-AC('Butter', 10);
-console.log(SC);
+// import { addToCart as AC, shoppingCart as SC } from './shoppingCart.js';
+// AC('Butter', 10);
+// console.log(SC);
 
+import * as ShoppingCart from './shoppingCart.js'; //importing all the exports at the sametime //ShoppingCart will be an Object and that will store all properties from shoppingCart.js
 console.log('Importing Module');
+
+ShoppingCart.addToCart('Butter', 10);
+console.log(ShoppingCart.totalPrice);
--- 17-Modules-Tooling-and-Functional/shoppingCart.js
+++ 17-Modules-Tooling-and-Functional/shoppingCart.js
@@ -1,11 +1,11 @@
 //exporting module
 console.log('Exporting Module');
 
-const shoppingCart = 10;
+const totalPrice = 10;
 const cart = [];
 const addToCart = function (item, quantity) {
   cart.push({ item, quantity });
   console.log(`${quantity} ${item} are added to the cart`);
 };
 
-// export { addToCart, shoppingCart }; // export should be used as the top level code, export-import works as live connection  //named exports
+export { addToCart, totalPrice }; // export should be used as the top level code, export-import works as live connection  //named exports
