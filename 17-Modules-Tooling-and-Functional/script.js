86891a631bb1fd51f02d4508596296bed6ff986f
shivPosted <shivpratap621@gmail.com>
2023-09-30 23:51:38 +0530

    installing dependencies using npm 😄

--- 17-Modules-Tooling-and-Functional/script.js
+++ 17-Modules-Tooling-and-Functional/script.js
@@ -11,3 +11,16 @@ console.log(ShoppingCart.totalPrice);
 
 import add from './shoppingCart.js'; //importing default export, don't use curylbraces for default exports
 add('burger', 5);
+
+import objToStr from './node_modules/lodash-es/_objectToString.js';
+
+// const obj01 = {
+//   name: 'Shiv',
+//   profession: 'student',
+//   stream: 'computer-science',
+//   hobby: 'exercise',
+// };
+const d01 = 23567895;
+
+const obj01res = objToStr(d01);
+console.log(obj01res);
